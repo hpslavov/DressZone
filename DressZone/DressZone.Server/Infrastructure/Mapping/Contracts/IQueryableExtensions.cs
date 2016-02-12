8490e55d2460afba4a848e2febd1794d77c922be
hpslavov <slavov.hristo@gmail.com>
2016-02-12 13:53:15 +0200

    Added Ninject IoC and Automapper.

---
+++ DressZone/DressZone.Server/Infrastructure/Mapping/Contracts/IQueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DressZone.Server.Infrastructure.Mapping.Contracts
+{
+    using AutoMapper.QueryableExtensions;
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<TDestination> To<TDestination>(this IQueryable source, params Expression<Func<TDestination, object>>[] membersToExpand)
+        {
+            return source.ProjectTo(AutoMapperConfig.Configuration, membersToExpand);
+        }
+    }
+}
